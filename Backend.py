f1ff87a59b15aefcaae2e2ccb2cd497c006986df
Adam IMA <59504028+IIAB-prog@users.noreply.github.com>
2025-12-20 08:49:15 +0000

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Éditeur de Spyder
-
-Ceci est un script temporaire.
-"""
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
-
-
+import os
 
 # Charger le modèle
 model = joblib.load("eligibilite_pret_model.pkl")
@@ -24,12 +18,20 @@ def home():
 # Route de prédiction
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)  # récupérer JSON
-    df = pd.DataFrame([data])            # transformer en DataFrame
+    data = request.get_json(force=True)
+    df = pd.DataFrame([data])
+
+    # Prédiction (Y / N)
     prediction = model.predict(df)[0]
-    return jsonify({"Loan_Status": prediction})
+
+    # Probabilité (classe positive = Y)
+    probability = model.predict_proba(df)[0][1]
+
+    return jsonify({
+        "Loan_Status": prediction,
+        "Probability": round(float(probability), 4)
+    })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render fournit le port dans $PORT
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
